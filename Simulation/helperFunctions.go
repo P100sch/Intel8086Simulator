15384bcd1831292612d3024ed0e313fc66cf5739
P100sch <ph.100sch@gmail.com>
2025-02-19 18:31:09 +0100

    add add, sub, cmp simulation

--- Simulation/helperFunctions.go
+++ Simulation/helperFunctions.go
@@ -1,169 +1,253 @@
 package Simulation
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"strings"
 )
 
 const (
-  _L = 0b0000000011111111
-  _H = 0b1111111100000000
+	_L      uint16 = 0b0000000011111111
+	_H      uint16 = 0b1111111100000000
+	_W_SIGN uint16 = 0b1000000000000000
+	_B_SIGN uint16 = 0b0000000010000000
+	_W_MAX  uint16 = 0b1111111111111111
+	_B_MAX  byte   = 0b11111111
 )
 
 func readH(w uint16) uint16 {
-  return w & _H >> 8
+	return w & _H >> 8
 }
 
 func writeL(w, b uint16) uint16 {
-  return w&_H | b&_L
+	return w&_H | b&_L
 }
 
 func writeH(w, b uint16) uint16 {
-  return w&_L | b<<8
+	return w&_L | b<<8
 }
 
 func readRegister(register byte) uint16 {
-  switch register {
-  case 0b0000:
-    return AX & _L
-  case 0b0001:
-    return CX & _L
-  case 0b0010:
-    return DX & _L
-  case 0b0011:
-    return BX & _L
-  case 0b0100:
-    return readH(AX)
-  case 0b0101:
-    return readH(CX)
-  case 0b0110:
-    return readH(DX)
-  case 0b0111:
-    return readH(BX)
-  case 0b1000:
-    return AX
-  case 0b1001:
-    return CX
-  case 0b1010:
-    return DX
-  case 0b1011:
-    return BX
-  case 0b1100:
-    return SP
-  case 0b1101:
-    return BP
-  case 0b1110:
-    return SI
-  case 0b1111:
-    return DI
-  default:
-    panic("Invalid register value")
-  }
+	switch register {
+	case 0b0000:
+		return AX & _L
+	case 0b0001:
+		return CX & _L
+	case 0b0010:
+		return DX & _L
+	case 0b0011:
+		return BX & _L
+	case 0b0100:
+		return readH(AX)
+	case 0b0101:
+		return readH(CX)
+	case 0b0110:
+		return readH(DX)
+	case 0b0111:
+		return readH(BX)
+	case 0b1000:
+		return AX
+	case 0b1001:
+		return CX
+	case 0b1010:
+		return DX
+	case 0b1011:
+		return BX
+	case 0b1100:
+		return SP
+	case 0b1101:
+		return BP
+	case 0b1110:
+		return SI
+	case 0b1111:
+		return DI
+	default:
+		panic("Invalid register value")
+	}
 }
 
 func writeRegister(register byte, value uint16) {
-  switch register {
-  case 0b0000:
-    AX = writeL(AX, value)
-  case 0b0001:
-    CX = writeL(CX, value)
-  case 0b0010:
-    DX = writeL(DX, value)
-  case 0b0011:
-    BX = writeL(BX, value)
-  case 0b0100:
-    AX = writeH(AX, value)
-  case 0b0101:
-    CX = writeH(CX, value)
-  case 0b0110:
-    DX = writeH(DX, value)
-  case 0b0111:
-    BX = writeH(BX, value)
-  case 0b1000:
-    AX = value
-  case 0b1001:
-    CX = value
-  case 0b1010:
-    DX = value
-  case 0b1011:
-    BX = value
-  case 0b1100:
-    SP = value
-  case 0b1101:
-    BP = value
-  case 0b1110:
-    SI = value
-  case 0b1111:
-    DI = value
-  default:
-    panic("Invalid register value")
-  }
+	switch register {
+	case 0b0000:
+		AX = writeL(AX, value)
+	case 0b0001:
+		CX = writeL(CX, value)
+	case 0b0010:
+		DX = writeL(DX, value)
+	case 0b0011:
+		BX = writeL(BX, value)
+	case 0b0100:
+		AX = writeH(AX, value)
+	case 0b0101:
+		CX = writeH(CX, value)
+	case 0b0110:
+		DX = writeH(DX, value)
+	case 0b0111:
+		BX = writeH(BX, value)
+	case 0b1000:
+		AX = value
+	case 0b1001:
+		CX = value
+	case 0b1010:
+		DX = value
+	case 0b1011:
+		BX = value
+	case 0b1100:
+		SP = value
+	case 0b1101:
+		BP = value
+	case 0b1110:
+		SI = value
+	case 0b1111:
+		DI = value
+	default:
+		panic("Invalid register value")
+	}
 }
 
 func readWord(data []byte, position int) uint16 {
-  return uint16(data[position-1]) | uint16(data[position])<<8
+	return uint16(data[position-1]) | uint16(data[position])<<8
 }
 
 func read(data []byte, position *int, wide bool) (uint16, error) {
-  *position++
-  if wide {
-    *position++
-  }
-  if *position >= len(data) {
-    return 0, newInvalidParameterErrorPrematureEndOfStream(*position)
-  }
-  if wide {
-    return readWord(data, *position), nil
-  }
-  return uint16(data[*position]), nil
+	*position++
+	if wide {
+		*position++
+	}
+	if *position >= len(data) {
+		return 0, newInvalidParameterErrorPrematureEndOfStream(*position)
+	}
+	if wide {
+		return readWord(data, *position), nil
+	}
+	return uint16(data[*position]), nil
 }
 
-func state() string {
-  builder := strings.Builder{}
-  if TF != 0 {
-    builder.WriteString("T")
-  } else {
-    builder.WriteByte(' ')
-  }
-  if DF != 0 {
-    builder.WriteString("D")
-  } else {
-    builder.WriteByte(' ')
-  }
-  if IF != 0 {
-    builder.WriteString("I")
-  } else {
-    builder.WriteByte(' ')
-  }
-  if OF != 0 {
-    builder.WriteString("O")
-  } else {
-    builder.WriteByte(' ')
-  }
-  if SF != 0 {
-    builder.WriteString("S")
-  } else {
-    builder.WriteByte(' ')
-  }
-  if ZF != 0 {
-    builder.WriteString("Z")
-  } else {
-    builder.WriteByte(' ')
-  }
-  if AF != 0 {
-    builder.WriteString("A")
-  } else {
-    builder.WriteByte(' ')
-  }
-  if PF != 0 {
-    builder.WriteString("P")
-  } else {
-    builder.WriteByte(' ')
-  }
-  if CF != 0 {
-    builder.WriteString("C")
-  } else {
-    builder.WriteByte(' ')
-  }
-  return fmt.Sprintf("AX:0x%04x BX:0x%04x CX:0x%04x DX:0x%04x SP:0x%04x BP:0x%04x SI:0x%04x DI:0x%04x IP:0x%04x CS:0x%04x DS:0x%04x SS:0x%04x ES:0x%04x F:%9s", AX, BX, CX, DX, SP, BP, SI, DI, IP, CS, DS, SS, ES, builder.String())
+func signExtend(x uint16) uint16 {
+	signExtension := x & 0b10000000 >> 7 * _H
+	return signExtension | x
+}
+
+func add(addend1, addend2 uint16, wide bool) uint16 {
+	var maxValue uint32
+	var signBit uint16
+	if wide {
+		maxValue = uint32(_W_MAX)
+		signBit = _W_SIGN
+	} else {
+		maxValue = uint32(_B_MAX)
+		signBit = _B_SIGN
+	}
+	sum := uint32(addend1)&maxValue + uint32(addend2)&maxValue
+	truncatedSum := uint16(sum & maxValue)
+
+	if sum > maxValue {
+		CF = 1
+	} else {
+		CF = 0
+	}
+	if (addend1&0b1111)+(addend2&0b1111) > 0b1111 {
+		AF = 1
+	} else {
+		AF = 0
+	}
+
+	sign1 := addend1 & signBit
+	sign2 := addend2 & signBit
+	signSum := truncatedSum & signBit
+	if sign1&sign2 == signSum || sign1|sign2 == signSum {
+		OF = 0
+	} else {
+		OF = 1
+	}
+	setCommonFlags(truncatedSum, signBit)
+	return truncatedSum
+}
+
+func sub(minuend, subtrahend uint16, wide bool) uint16 {
+	var maxValue uint16
+	if wide {
+		maxValue = _W_MAX
+	} else {
+		maxValue = uint16(_B_MAX)
+	}
+	negatedSubtrahend := subtrahend ^ maxValue + 1
+	difference := add(minuend, negatedSubtrahend, wide)
+
+	//Carry/AuxCarry/Overflow flags for subtraction are the opposite to the addition flags
+	CF = CF ^ 1
+	AF = AF ^ 1
+
+	return difference
+}
+
+func setCommonFlags(value uint16, signBit uint16) {
+	if value == 0 {
+		ZF = 1
+	} else {
+		ZF = 0
+	}
+	odd := (value & 0b11110000 >> 4) ^ (value & 0b1111)
+	odd = (odd & 0b1100 >> 2) ^ (odd & 0b11)
+	odd = (odd & 0b10 >> 1) ^ (odd & 0b1)
+	if odd == 1 {
+		PF = 0
+	} else {
+		PF = 1
+	}
+	if value&signBit == 0 {
+		SF = 0
+	} else {
+		SF = 1
+	}
+}
+
+// formatState formats the state in a loggable format.
+func formatState() string {
+	builder := strings.Builder{}
+	if TF != 0 {
+		builder.WriteString("T")
+	} else {
+		builder.WriteByte(' ')
+	}
+	if DF != 0 {
+		builder.WriteString("D")
+	} else {
+		builder.WriteByte(' ')
+	}
+	if IF != 0 {
+		builder.WriteString("I")
+	} else {
+		builder.WriteByte(' ')
+	}
+	if OF != 0 {
+		builder.WriteString("O")
+	} else {
+		builder.WriteByte(' ')
+	}
+	if SF != 0 {
+		builder.WriteString("S")
+	} else {
+		builder.WriteByte(' ')
+	}
+	if ZF != 0 {
+		builder.WriteString("Z")
+	} else {
+		builder.WriteByte(' ')
+	}
+	if AF != 0 {
+		builder.WriteString("A")
+	} else {
+		builder.WriteByte(' ')
+	}
+	if PF != 0 {
+		builder.WriteString("P")
+	} else {
+		builder.WriteByte(' ')
+	}
+	if CF != 0 {
+		builder.WriteString("C")
+	} else {
+		builder.WriteByte(' ')
+	}
+	return fmt.Sprintf("AX:0x%04x BX:0x%04x CX:0x%04x DX:0x%04x SP:0x%04x BP:0x%04x SI:0x%04x DI:0x%04x IP:0x%04x CS:0x%04x DS:0x%04x SS:0x%04x ES:0x%04x F:%9s", AX, BX, CX, DX, SP, BP, SI, DI, IP, CS, DS, SS, ES, builder.String())
 }
--- Simulation/simulator.go
+++ Simulation/simulator.go
@@ -1,38 +1,38 @@
 package Simulation
 
 import (
-  "log"
-  "strconv"
+	"log"
+	"strconv"
 
-  "github.com/P100sch/Intel8086Simulator/Simulation/Disassembly"
-  "github.com/P100sch/Intel8086Simulator/Simulation/Shared"
+	"github.com/P100sch/Intel8086Simulator/Simulation/Disassembly"
+	"github.com/P100sch/Intel8086Simulator/Simulation/Shared"
 )
 
 //region Types
 
 type DecodingError struct {
-  Message string
-  Pos     int
+	Message string
+	Pos     int
 }
 
 func (e *DecodingError) Error() string {
-  return "Position " + strconv.Itoa(e.Pos) + ": " + e.Message
+	return "Position " + strconv.Itoa(e.Pos) + ": " + e.Message
 }
 
 func newInvalidParameterError(position int, cause string) *DecodingError {
-  return &DecodingError{Message: "invalid parameters (" + cause + ")", Pos: position}
+	return &DecodingError{Message: "invalid parameters (" + cause + ")", Pos: position}
 }
 
 func newInvalidParameterErrorPrematureEndOfStream(position int) *DecodingError {
-  return newInvalidParameterError(position, "reached end of instruction stream while decoding")
+	return newInvalidParameterError(position, "reached end of instruction stream while decoding")
 }
 
 func newInvalidParameterErrorInvalidInstruction(position int) *DecodingError {
-  return newInvalidParameterError(position, "invalid instruction in register portion")
+	return newInvalidParameterError(position, "invalid instruction in register portion")
 }
 
 func newUnsupportedError(position int, reason string) *DecodingError {
-  return &DecodingError{Message: "unsupported function (" + reason + ")", Pos: position}
+	return &DecodingError{Message: "unsupported function (" + reason + ")", Pos: position}
 }
 
 //endregion
@@ -45,194 +45,355 @@ func newUnsupportedError(position int, reason string) *DecodingError {
 //
 //goland:noinspection SpellCheckingInspection
 func Simulate(fileData []byte, logger *log.Logger) error {
-  var err error
-  dataLength := len(fileData)
-  startOfInstruction := 0
+	var err error
+	dataLength := len(fileData)
+	startOfInstruction := 0
 
-  var sourceValue uint16
+	var sourceValue uint16
 
-  for position := 0; position < dataLength; position++ {
-    switch fileData[position] {
+	for position := 0; position < dataLength; position++ {
+		switch fileData[position] {
 
-    //Standard MOV permutations
-    case 0b10001000:
-      fallthrough
-    case 0b10001001:
-      wide := Shared.IsolateAndShiftWide(fileData[position])
-      position++
-      if position == dataLength {
-        return newInvalidParameterErrorPrematureEndOfStream(position)
-      }
-      sourceValue = readRegister(wide | fileData[position]&Shared.RegMask>>3)
-      if fileData[position]&Shared.ModMask == Shared.RegisterMode {
-        writeRegister(wide|fileData[position]&Shared.RMMask, sourceValue)
-      } else {
-        return newUnsupportedError(position, "memory not implemented")
-      }
-    case 0b10001010:
-      fallthrough
-    case 0b10001011:
-      wide := Shared.IsolateAndShiftWide(fileData[position])
-      position++
-      if position == dataLength {
-        return newInvalidParameterErrorPrematureEndOfStream(position)
-      }
-      if fileData[position]&Shared.ModMask == Shared.RegisterMode {
-        sourceValue = readRegister(wide | fileData[position]&Shared.RMMask)
-      } else {
-        return newUnsupportedError(position, "memory not implemented")
-      }
-      writeRegister(wide|fileData[position]&Shared.RegMask>>3, sourceValue)
-    //MOV segment register to R/M
-    case 0b10001100:
-      position++
-      if position == dataLength {
-        return newInvalidParameterErrorPrematureEndOfStream(position)
-      }
-      switch fileData[position] & Shared.RegMask {
-      case 0b000000:
-        sourceValue = ES
-      case 0b001000:
-        sourceValue = CS
-      case 0b010000:
-        sourceValue = SS
-      case 0b011000:
-        sourceValue = DS
-      default:
-        return newInvalidParameterErrorInvalidInstruction(position)
-      }
-      if fileData[position]&Shared.ModMask == Shared.RegisterMode {
-        writeRegister(Shared.WIDE|fileData[position]&Shared.RMMask, sourceValue)
-      } else {
-        return newUnsupportedError(position, "memory not implemented")
-      }
-    //MOV R/M to segment register
-    case 0b10001110:
-      position++
-      if position == dataLength {
-        return newInvalidParameterErrorPrematureEndOfStream(position)
-      }
-      if fileData[position]&Shared.ModMask == Shared.RegisterMode {
-        sourceValue = readRegister(Shared.WIDE | fileData[position]&Shared.RMMask)
-      } else {
-        return newUnsupportedError(position, "memory not implemented")
-      }
-      switch fileData[position] & Shared.RegMask {
-      case 0b000000:
-        ES = sourceValue
-      case 0b001000:
-        CS = sourceValue
-      case 0b010000:
-        SS = sourceValue
-      case 0b011000:
-        DS = sourceValue
-      default:
-        return newInvalidParameterErrorInvalidInstruction(position)
-      }
-    //MOV immediate
-    case 0b11000110:
-      fallthrough
-    case 0b11000111:
-      wide := Shared.IsolateAndShiftWide(fileData[position])
-      position++
-      if position == dataLength {
-        return newInvalidParameterErrorPrematureEndOfStream(position)
-      }
-      if fileData[position]&Shared.RegMask != 0 {
-        return newInvalidParameterErrorInvalidInstruction(position)
-      }
-      rmBits := fileData[position] & Shared.RMMask
-      sourceValue, err = read(fileData, &position, wide != 0)
-      if err != nil {
-        return err
-      }
-      if fileData[position]&Shared.ModMask == Shared.RegisterMode {
-        writeRegister(wide|rmBits, sourceValue)
-      } else {
-        return newUnsupportedError(position, "memory not implemented")
-      }
-    //MOV immediate into register
-    case 0b10110000:
-      fallthrough
-    case 0b10110001:
-      fallthrough
-    case 0b10110010:
-      fallthrough
-    case 0b10110011:
-      fallthrough
-    case 0b10110100:
-      fallthrough
-    case 0b10110101:
-      fallthrough
-    case 0b10110110:
-      fallthrough
-    case 0b10110111:
-      fallthrough
-    case 0b10111000:
-      fallthrough
-    case 0b10111001:
-      fallthrough
-    case 0b10111010:
-      fallthrough
-    case 0b10111011:
-      fallthrough
-    case 0b10111100:
-      fallthrough
-    case 0b10111101:
-      fallthrough
-    case 0b10111110:
-      fallthrough
-    case 0b10111111:
-      register := fileData[position] & 0b00001111
-      sourceValue, err = read(fileData, &position, register > 0b0111)
-      if err != nil {
-        return err
-      }
-      writeRegister(register, sourceValue)
+		//Standard MOV permutations
+		case 0b10001000:
+			fallthrough
+		case 0b10001001:
+			wide := Shared.IsolateAndShiftWide(fileData[position])
+			position++
+			if position == dataLength {
+				return newInvalidParameterErrorPrematureEndOfStream(position)
+			}
+			sourceValue = readRegister(wide | fileData[position]&Shared.RegMask>>3)
+			if fileData[position]&Shared.ModMask == Shared.RegisterMode {
+				writeRegister(wide|fileData[position]&Shared.RMMask, sourceValue)
+			} else {
+				return newUnsupportedError(position, "memory not implemented")
+			}
+		case 0b10001010:
+			fallthrough
+		case 0b10001011:
+			wide := Shared.IsolateAndShiftWide(fileData[position])
+			position++
+			if position == dataLength {
+				return newInvalidParameterErrorPrematureEndOfStream(position)
+			}
+			if fileData[position]&Shared.ModMask == Shared.RegisterMode {
+				sourceValue = readRegister(wide | fileData[position]&Shared.RMMask)
+			} else {
+				return newUnsupportedError(position, "memory not implemented")
+			}
+			writeRegister(wide|fileData[position]&Shared.RegMask>>3, sourceValue)
+		//MOV segment register to R/M
+		case 0b10001100:
+			position++
+			if position == dataLength {
+				return newInvalidParameterErrorPrematureEndOfStream(position)
+			}
+			switch fileData[position] & Shared.RegMask {
+			case 0b000000:
+				sourceValue = ES
+			case 0b001000:
+				sourceValue = CS
+			case 0b010000:
+				sourceValue = SS
+			case 0b011000:
+				sourceValue = DS
+			default:
+				return newInvalidParameterErrorInvalidInstruction(position)
+			}
+			if fileData[position]&Shared.ModMask == Shared.RegisterMode {
+				writeRegister(Shared.WIDE|fileData[position]&Shared.RMMask, sourceValue)
+			} else {
+				return newUnsupportedError(position, "memory not implemented")
+			}
+		//MOV R/M to segment register
+		case 0b10001110:
+			position++
+			if position == dataLength {
+				return newInvalidParameterErrorPrematureEndOfStream(position)
+			}
+			if fileData[position]&Shared.ModMask == Shared.RegisterMode {
+				sourceValue = readRegister(Shared.WIDE | fileData[position]&Shared.RMMask)
+			} else {
+				return newUnsupportedError(position, "memory not implemented")
+			}
+			switch fileData[position] & Shared.RegMask {
+			case 0b000000:
+				ES = sourceValue
+			case 0b001000:
+				CS = sourceValue
+			case 0b010000:
+				SS = sourceValue
+			case 0b011000:
+				DS = sourceValue
+			default:
+				return newInvalidParameterErrorInvalidInstruction(position)
+			}
+		//MOV immediate
+		case 0b11000110:
+			fallthrough
+		case 0b11000111:
+			wide := Shared.IsolateAndShiftWide(fileData[position])
+			position++
+			if position == dataLength {
+				return newInvalidParameterErrorPrematureEndOfStream(position)
+			}
+			if fileData[position]&Shared.RegMask != 0 {
+				return newInvalidParameterErrorInvalidInstruction(position)
+			}
+			rmBits := fileData[position] & Shared.RMMask
+			sourceValue, err = read(fileData, &position, wide != 0)
+			if err != nil {
+				return err
+			}
+			if fileData[position]&Shared.ModMask == Shared.RegisterMode {
+				writeRegister(wide|rmBits, sourceValue)
+			} else {
+				return newUnsupportedError(position, "memory not implemented")
+			}
+		//MOV immediate into register
+		case 0b10110000:
+			fallthrough
+		case 0b10110001:
+			fallthrough
+		case 0b10110010:
+			fallthrough
+		case 0b10110011:
+			fallthrough
+		case 0b10110100:
+			fallthrough
+		case 0b10110101:
+			fallthrough
+		case 0b10110110:
+			fallthrough
+		case 0b10110111:
+			fallthrough
+		case 0b10111000:
+			fallthrough
+		case 0b10111001:
+			fallthrough
+		case 0b10111010:
+			fallthrough
+		case 0b10111011:
+			fallthrough
+		case 0b10111100:
+			fallthrough
+		case 0b10111101:
+			fallthrough
+		case 0b10111110:
+			fallthrough
+		case 0b10111111:
+			register := fileData[position] & 0b00001111
+			sourceValue, err = read(fileData, &position, register > 0b0111)
+			if err != nil {
+				return err
+			}
+			writeRegister(register, sourceValue)
 
-    default:
-      return newUnsupportedError(position, "unsupported instruction")
-      //return &DecodingError{Message: "invalid instruction", Pos: position}
-    }
+		//ADD/OR/ADC/SUB/AND/SBB/CMP immediate to R/M
+		case 0b10000000:
+			fallthrough
+		case 0b10000001:
+			fallthrough
+		case 0b10000010:
+			fallthrough
+		case 0b10000011:
+			signExtended := fileData[position]&Shared.DirectionMask != 0
+			wide := Shared.IsolateAndShiftWide(fileData[position])
+			position++
+			if position == dataLength {
+				return newInvalidParameterErrorPrematureEndOfStream(position)
+			}
+			operation := fileData[position] & Shared.RegMask
+			register := fileData[position] & Shared.RMMask
+			if fileData[position]&Shared.ModMask != Shared.RegisterMode {
+				return newUnsupportedError(position, "memory not implemented")
+			}
+			var immediate uint16
+			immediate, err = read(fileData, &position, wide != 0 && !signExtended)
+			if err != nil {
+				return err
+			}
+			if signExtended {
+				immediate = signExtend(immediate)
+			}
+			//var name = [8]string{"ADD ", "OR ", "ADC ", "SBB ", "AND ", "SUB ", "XOR ", "CMP "}[fileData[position]&Shared.RegMask>>3]
+			switch operation {
+			case 0b000000:
+				writeRegister(wide|register, add(readRegister(wide|register), immediate, wide != 0))
+			case 0b101000:
+				writeRegister(wide|register, sub(readRegister(wide|register), immediate, wide != 0))
+			case 0b111000:
+				_ = sub(readRegister(wide|register), immediate, wide != 0)
+			default:
+				return newUnsupportedError(position, "operation not implemented")
+			}
 
-    IP = uint16(position + 1)
-    logStateAndInstruction(fileData[startOfInstruction:position+1], logger)
-    startOfInstruction = position + 1
-  }
+		//ADD register with R/M
+		case 0b00000000:
+			fallthrough
+		case 0b00000001:
+			fallthrough
+		case 0b00000010:
+			fallthrough
+		case 0b00000011:
+			sourceInReg := fileData[position]&Shared.DirectionMask == 0
+			wide := Shared.IsolateAndShiftWide(fileData[position])
+			position++
+			if position == dataLength {
+				return newInvalidParameterErrorPrematureEndOfStream(position)
+			}
+			if fileData[position]&Shared.ModMask != Shared.RegisterMode {
+				return newUnsupportedError(position, "memory not implemented")
+			}
+			reg := wide | (fileData[position] & Shared.RegMask >> 3)
+			regValue := readRegister(reg)
+			rmRegister := wide | fileData[position]&Shared.RMMask
+			rmValue := readRegister(wide | rmRegister)
+			sum := add(regValue, rmValue, wide != 0)
+			if sourceInReg {
+				writeRegister(rmRegister, sum)
+			} else {
+				writeRegister(reg, sum)
+			}
+		//ADD immediate to accumulator
+		case 0b00000100:
+			fallthrough
+		case 0b00000101:
+			wide := fileData[position]&Shared.WideMask != 0
+			sourceValue, err = read(fileData, &position, wide)
+			if err != nil {
+				return err
+			}
+			sum := add(AX, sourceValue, wide)
+			if wide {
+				AX = sum
+			} else {
+				AX = writeL(AX, sum)
+			}
 
-  return nil
+		//SUB register and R/M
+		case 0b00101000:
+			fallthrough
+		case 0b00101001:
+			fallthrough
+		case 0b00101010:
+			fallthrough
+		case 0b00101011:
+			sourceInReg := fileData[position]&Shared.DirectionMask == 0
+			wide := Shared.IsolateAndShiftWide(fileData[position])
+			position++
+			if position == dataLength {
+				return newInvalidParameterErrorPrematureEndOfStream(position)
+			}
+			if fileData[position]&Shared.ModMask != Shared.RegisterMode {
+				return newUnsupportedError(position, "memory not implemented")
+			}
+			reg := wide | (fileData[position] & Shared.RegMask >> 3)
+			regValue := readRegister(reg)
+			rmRegister := wide | fileData[position]&Shared.RMMask
+			rmValue := readRegister(wide | rmRegister)
+			if sourceInReg {
+				writeRegister(rmRegister, sub(rmValue, regValue, wide != 0))
+			} else {
+				writeRegister(reg, sub(regValue, rmValue, wide != 0))
+			}
+		//SUB immediate from accumulator
+		case 0b00101100:
+			fallthrough
+		case 0b00101101:
+			wide := fileData[position]&Shared.WideMask != 0
+			sourceValue, err = read(fileData, &position, wide)
+			if err != nil {
+				return err
+			}
+			difference := sub(AX, sourceValue, wide)
+			if wide {
+				AX = difference
+			} else {
+				AX = writeL(AX, difference)
+			}
+
+		//CMP register to R/M
+		case 0b00111000:
+			fallthrough
+		case 0b00111001:
+			fallthrough
+		case 0b00111010:
+			fallthrough
+		case 0b00111011:
+			sourceInReg := fileData[position]&Shared.DirectionMask == 0
+			wide := Shared.IsolateAndShiftWide(fileData[position])
+			position++
+			if position == dataLength {
+				return newInvalidParameterErrorPrematureEndOfStream(position)
+			}
+			if fileData[position]&Shared.ModMask != Shared.RegisterMode {
+				return newUnsupportedError(position, "memory not implemented")
+			}
+			regValue := readRegister(wide | (fileData[position] & Shared.RegMask >> 3))
+			rmRegister := wide | fileData[position]&Shared.RMMask
+			rmValue := readRegister(wide | rmRegister)
+			sourceValue = readRegister(wide | rmRegister)
+			if sourceInReg {
+				_ = sub(rmValue, regValue, wide != 0)
+			} else {
+				_ = sub(regValue, rmValue, wide != 0)
+			}
+		//CMP immediate with accumulator
+		case 0b00111100:
+			fallthrough
+		case 0b00111101:
+			wide := fileData[position]&Shared.WideMask != 0
+			sourceValue, err = read(fileData, &position, wide)
+			if err != nil {
+				return err
+			}
+			_ = sub(AX, sourceValue, wide)
+
+		default:
+			return newUnsupportedError(position, "unsupported instruction")
+			//return &DecodingError{Message: "invalid instruction", Pos: position}
+		}
+
+		IP = uint16(position + 1)
+		logStateAndInstruction(fileData[startOfInstruction:position+1], logger)
+		startOfInstruction = position + 1
+	}
+
+	return nil
 }
 
 func Rest() {
-  AX = 0
-  BX = 0
-  CX = 0
-  DX = 0
-  SP = 0
-  BP = 0
-  SI = 0
-  DI = 0
-  IP = 0
-  CS = 0
-  DS = 0
-  SS = 0
-  ES = 0
-  TF = 0
-  DF = 0
-  IF = 0
-  OF = 0
-  SF = 0
-  ZF = 0
-  AF = 0
+	AX = 0
+	BX = 0
+	CX = 0
+	DX = 0
+	SP = 0
+	BP = 0
+	SI = 0
+	DI = 0
+	IP = 0
+	CS = 0
+	DS = 0
+	SS = 0
+	ES = 0
+	TF = 0
+	DF = 0
+	IF = 0
+	OF = 0
+	SF = 0
+	ZF = 0
+	AF = 0
 }
 
 func logStateAndInstruction(instruction []byte, logger *log.Logger) {
-  if logger != nil {
-    assembly, err := Disassembly.Disassemble(instruction)
-    if err != nil {
-      logger.Println(err.Error())
-    } else {
-      logger.Println(state() + " ; " + assembly)
-    }
-  }
+	if logger != nil {
+		assembly, err := Disassembly.Disassemble(instruction)
+		if err != nil {
+			logger.Println(err.Error())
+		} else {
+			logger.Println(formatState() + " ; " + assembly)
+		}
+	}
 }
